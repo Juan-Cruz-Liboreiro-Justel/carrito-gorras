8550ff2641304b60a9b20af94b4d4d5397a5b369
Juan Cruz Liboreiro Justel <liboreiro.juancruz@gmail.com>
2023-01-17 20:21:06 -0300

    cambios 3er preetrega

--- js/main.js
+++ js/main.js
@@ -1,118 +1,301 @@
-const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
-addToShoppingCartButtons.forEach((addToCartButton) => {
-  addToCartButton.addEventListener('click', addToCartClicked);
+const productos = [
+  // 59fifty
+  {
+      id: "Detroit Tigers 59FIFTY Citrus Pop",
+      titulo: "Detroit Tigers 59FIFTY Citrus Pop",
+      imagen: "./img/59fifty/detroit/1.jpg",
+      categoria: {
+          nombre: "59fifty",
+          id: "59fifty"
+      },
+      precio: 19499
+  },
+  {
+    id: "Los Angeles Dodgers 59FIFTY Citrus Pop",
+    titulo: "Los Angeles Dodgers 59FIFTY Citrus Pop",
+    imagen: "./img/59fifty/los-angeles/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},{
+    id: "New York Yankees 59FIFTY Diamond Era",
+    titulo: "New York Yankees 59FIFTY Diamond Era",
+    imagen: "./img/59fifty/new-york/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 17599
+},{
+    id: "Boston Red Sox 59FIFTY Citrus Pop",
+    titulo: "Boston Red Sox 59FIFTY Citrus Pop",
+    imagen: "./img/59fifty/boston/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},{
+    id: "San Francisco Giants 59FIFTY Citrus Pop",
+    titulo: "San Francisco Giants 59FIFTY Citrus Pop",
+    imagen: "./img/59fifty/san-francisco/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},{
+    id: "Philadelphia Phillies 59FIFTY Citrus Pop",
+    titulo: "Philadelphia Phillies 59FIFTY Citrus Pop",
+    imagen: "./img/59fifty/philadelphia/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},{
+    id: "New York Mets 59FIFTY Citrus Pop",
+    titulo: "New York Mets 59FIFTY Citrus Pop",
+    imagen: "./img/59fifty//new-york-mets/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},{
+    id: "Miami Heat 59FIFTY Dual Logo",
+    titulo: "Miami Heat 59FIFTY Dual Logo",
+    imagen: "./img/59fifty//miami/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},{
+    id: "Golden State Warriors 59FIFTY Citrus Pop",
+    titulo: "Golden State Warriors 59FIFTY Citrus Pop",
+    imagen: "./img/59fifty/golden-state/1.jpg",
+    categoria: {
+        nombre: "59fifty",
+        id: "59fifty"
+    },
+    precio: 19499
+},
+  // 9forty
+  {
+      id: "Manchester United FC 9FORTY",
+      titulo: "Manchester United FC 9FORTY",
+      imagen: "./img/9forty/manchester/1.jpg",
+      categoria: {
+          nombre: "9forty",
+          id: "9forty"
+      },
+      precio: 12299
+  },
+  {
+    id: "Tottenham Hotspur FC 9FORTY",
+    titulo: "Tottenham Hotspur FC 9FORTY",
+    imagen: "./img/9forty/tottenham/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},{
+    id: "Ducati Motor Logo Tonal Camo Mesh 9FORTY",
+    titulo: "Ducati Motor Logo Tonal Camo Mesh 9FORTY",
+    imagen: "./img/9forty/ducati/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},{
+    id: "Los Angeles Lakers 9FORTY NBA Grayscale",
+    titulo: "Los Angeles Lakers 9FORTY NBA Grayscale",
+    imagen: "./img/9forty/lakers/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},{
+    id: "Chelsea FC Lion Crest Bob Ripstop 9FORTY",
+    titulo: "Chelsea FC Lion Crest Bob Ripstop 9FORTY",
+    imagen: "./img/9forty/chealse/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},{
+    id: "Houston Rockets Era 9FORTY",
+    titulo: "Houston Rockets Era 9FORTY",
+    imagen: "./img/9forty/houston/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},{
+    id: "Los Angeles Dodgers Diamond Era 9FORTY",
+    titulo: "Los Angeles Dodgers Diamond Era 9FORTY",
+    imagen: "./img/9forty/dodgers/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},{
+    id: "New York Yankees 9FORTY Tonal Mesh Trucker",
+    titulo: "New York Yankees 9FORTY Tonal Mesh Trucker",
+    imagen: "./img/9forty/yankees/1.jpg",
+    categoria: {
+        nombre: "9forty",
+        id: "9forty"
+    },
+    precio: 12299
+},
+  // 39thirty
+  {
+      id: "Jacksonville Jaguars NFL22 Sideline 39THIRTY",
+      titulo: "Jacksonville Jaguars NFL22 Sideline 39THIRTY",
+      imagen: "./img/39thirty/jacksonville/1.jpg",
+      categoria: {
+          nombre: "39thirty",
+          id: "39thirty"
+      },
+      precio: 13599
+  },{
+    id: "Chicago Bears NFL22 Sideline 39THIRTY",
+    titulo: "Chicago Bears NFL22 Sideline 39THIRTY",
+    imagen: "./img/39thirty/chicago/1.jpg",
+    categoria: {
+        nombre: "39thirty",
+        id: "39thirty"
+    },
+    precio: 13599
+},{
+    id: "Brooklyn Nets Team Classic 39THIRTY",
+    titulo: "Brooklyn Nets Team Classic 39THIRTY",
+    imagen: "./img/39thirty/brooklyn/1.jpg",
+    categoria: {
+        nombre: "39thirty",
+        id: "39thirty"
+    },
+    precio: 13599
+},{
+    id: "New England Patriots NFL22 Sideline 39THIRTY",
+    titulo: "New England Patriots NFL22 Sideline 39THIRTY",
+    imagen: "./img/39thirty/new-england/1.jpg",
+    categoria: {
+        nombre: "39thirty",
+        id: "39thirty"
+    },
+    precio: 13599
+},{
+    id: "Tampa Bay Buccaneers NFL22 Sideline 39THIRTY",
+    titulo: "Tampa Bay Buccaneers NFL22 Sideline 39THIRTY",
+    imagen: "./img/39thirty/tampa/1.jpg",
+    categoria: {
+        nombre: "39thirty",
+        id: "39thirty"
+    },
+    precio: 13599
+},
+];
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+function cargarProductos(productosElegidos) {
+
+  contenedorProductos.innerHTML = "";
+
+  productosElegidos.forEach(producto => {
+
+      const div = document.createElement("div");
+      div.classList.add("producto");
+      div.innerHTML = `
+          <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+          <div class="producto-detalles">
+              <h3 class="producto-titulo">${producto.titulo}</h3>
+              <p class="producto-precio">$${producto.precio}</p>
+              <button class="producto-agregar" id="${producto.id}">Agregar</button>
+          </div>
+      `;
+
+      contenedorProductos.append(div);
+  })
+
+  actualizarBotonesAgregar();
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+  boton.addEventListener("click", (e) => {
+
+      botonesCategorias.forEach(boton => boton.classList.remove("active"));
+      e.currentTarget.classList.add("active");
+
+      if (e.currentTarget.id != "todos") {
+          const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+          tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+          const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+          cargarProductos(productosBoton);
+      } else {
+          tituloPrincipal.innerText = "Todos los productos";
+          cargarProductos(productos);
+      }
+
+  })
 });
 
-const comprarButton = document.querySelector('.comprarButton');
-comprarButton.addEventListener('click', comprarButtonClicked);
+function actualizarBotonesAgregar() {
+  botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-const shoppingCartItemsContainer = document.querySelector(
-  '.shoppingCartItemsContainer'
-);
-
-function addToCartClicked(event) {
-  const button = event.target;
-  const item = button.closest('.item');
-
-  const itemTitle = item.querySelector('.item-title').textContent;
-  const itemPrice = item.querySelector('.item-price').textContent;
-  const itemImage = item.querySelector('.item-image').src;
-
-  addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+  botonesAgregar.forEach(boton => {
+      boton.addEventListener("click", agregarAlCarrito);
+  });
 }
 
-function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
-  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
-    'shoppingCartItemTitle'
-  );
-  for (let i = 0; i < elementsTitle.length; i++) {
-    if (elementsTitle[i].innerText === itemTitle) {
-      let elementQuantity = elementsTitle[
-        i
-      ].parentElement.parentElement.parentElement.querySelector(
-        '.shoppingCartItemQuantity'
-      );
-      elementQuantity.value++;
-      $('.toast').toast('show');
-      updateShoppingCartTotal();
-      return;
-    }
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+  productosEnCarrito = JSON.parse(productosEnCarritoLS);
+  actualizarNumerito();
+} else {
+  productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+  if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+      const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+      productosEnCarrito[index].cantidad++;
+  } else {
+      productoAgregado.cantidad = 1;
+      productosEnCarrito.push(productoAgregado);
   }
 
-  const shoppingCartRow = document.createElement('div');
-  const shoppingCartContent = `
-  <div class="row shoppingCartItem">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
-    </div>`;
-  shoppingCartRow.innerHTML = shoppingCartContent;
-  shoppingCartItemsContainer.append(shoppingCartRow);
+  actualizarNumerito();
 
-  shoppingCartRow
-    .querySelector('.buttonDelete')
-    .addEventListener('click', removeShoppingCartItem);
-
-  shoppingCartRow
-    .querySelector('.shoppingCartItemQuantity')
-    .addEventListener('change', quantityChanged);
-
-  updateShoppingCartTotal();
+  localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
 
-function updateShoppingCartTotal() {
-  let total = 0;
-  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
-
-  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
-
-  shoppingCartItems.forEach((shoppingCartItem) => {
-    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemPrice'
-    );
-    const shoppingCartItemPrice = Number(
-      shoppingCartItemPriceElement.textContent.replace('$', '')
-    );
-    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemQuantity'
-    );
-    const shoppingCartItemQuantity = Number(
-      shoppingCartItemQuantityElement.value
-    );
-    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
-  });
-  shoppingCartTotal.innerHTML = `$${total.toFixed(2)}`;
-}
-
-function removeShoppingCartItem(event) {
-  const buttonClicked = event.target;
-  buttonClicked.closest('.shoppingCartItem').remove();
-  updateShoppingCartTotal();
-}
-
-function quantityChanged(event) {
-  const input = event.target;
-  input.value <= 0 ? (input.value = 1) : null;
-  updateShoppingCartTotal();
-}
-
-function comprarButtonClicked() {
-  shoppingCartItemsContainer.innerHTML = '';
-  updateShoppingCartTotal();
-}
+function actualizarNumerito() {
+  let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+  numerito.innerText = nuevoNumerito;
+} 
